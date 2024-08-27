318895ba2127d90998c9eb61594a71a6d153a25a
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-27 20:47:27 +0800

    add demo

---
+++ awesomeProject/src/Demo/main.go
@@ -0,0 +1,16 @@
+package main
+
+//func main() {
+//	fmt.Println("hello world")
+//	// 变量
+//	var (
+//		num1 int
+//		num  = 18
+//		num2 = "tom"//也可以声明变量的类型
+//	)
+//	sex := "男"
+//	fmt.Println(num1)
+//	fmt.Println(num)
+//	fmt.Println(num2)
+//	fmt.Println(sex)
+//}
