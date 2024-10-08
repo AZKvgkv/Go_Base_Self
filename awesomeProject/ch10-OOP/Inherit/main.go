4f70f1cd05eaf8a97175d4186002f87f7a5c32c6
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-10-08 12:07:44 +0800

    update

--- awesomeProject/ch10-OOP/Inherit/main.go
+++ awesomeProject/ch10-OOP/Inherit/main.go
@@ -12,9 +12,10 @@ type Animal struct {
 func (an *Animal) Shout() {
 	println("动物叫")
 }
+
 // 给动物结构体添加方法 自我展示
 func (an *Animal) ShowInfo() {
-	fmt.Printf("我是一只动物，我的年龄是%v，我的体重是%v\n",an.Age, an.Weight)
+	fmt.Printf("我是一只动物，我的年龄是%v，我的体重是%v\n", an.Age, an.Weight)
 }
 
 // 定义猫结构体
@@ -32,15 +33,7 @@ func (c *Cat) EatFish() {
 	println("猫吃鱼")
 }
 
-
 func main() {
-	// 实例化一个猫
-	// cat := Cat{Animal{2, 3.5}, "橘色"}
-	// // 调用猫的方法
-	// cat.Shout()
-	// cat.ShowInfo()
-	// cat.Run()
-	// cat.EatFish()
 	cat := &Cat{}
 	cat.Animal.Age = 2
 	cat.Animal.Weight = 3.5
